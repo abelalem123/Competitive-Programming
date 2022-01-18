cf9c4694ee8143f62408d29f839a6b4cf9aea5af
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2022-01-18 11:33:56 +0300

    Create DominoPilling.java

---
+++ DominoPilling.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class JavaApplication41 {
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int k=n*m/2;
+        System.out.println(k);
+       
+    }
+    
+}
