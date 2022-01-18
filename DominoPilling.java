19d6dd18d3c642e0a3899a2f7ca4142e55876805
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2022-01-18 11:35:08 +0300

    Update DominoPilling.java

--- DominoPilling.java
+++ DominoPilling.java
@@ -1,15 +1,15 @@
 
 import java.util.Scanner;
 
-public class JavaApplication41 {
+public class A2SV {
  
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int m=sc.nextInt();
-        int k=n*m/2;
-        System.out.println(k);
+        int ans=n*m/2;
+        System.out.println(ans);
        
     }
     
