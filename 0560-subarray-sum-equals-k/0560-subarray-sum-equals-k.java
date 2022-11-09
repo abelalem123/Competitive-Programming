c186b1bfd90d959b97c162f3f1f98f2730f69310
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2022-11-09 19:23:04 +0300

    Time: 21 ms (99.02%), Space: 47.2 MB (81.50%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,129 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+      
+ HashMap<Integer,Integer>sums=new HashMap();
+        sums.put(0,1);
+        int sum=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sums.containsKey(sum-k)){
+                count+=sums.get(sum-k);
+            }
+            sums.put(sum,sums.getOrDefault(sum,0)+1);
+        }
+        
+        return count;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   HashMap<Integer,Integer> sums=new HashMap();
+//         sums.put(0,1);
+//         int sum=0;
+//         int ans=0;
+//         for(int i=0;i<nums.length;i++){
+//             sum+=nums[i];
+        
+//             if(sums.containsKey(sum-k)){
+//                 ans+=sums.get(sum-k);
+//             }
+//             sums.put(sum,sums.getOrDefault(sum,0)+1);
+//         }
+//         return ans;
