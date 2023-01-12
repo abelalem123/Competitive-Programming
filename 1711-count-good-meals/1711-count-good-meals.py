6792d2b9c510e1e80fc3b471345b04127427ccb5
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-01-12 15:23:02 +0300

    Time: 937 ms (86.91%), Space: 21.9 MB (50.43%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.py
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -1,16 +1,13 @@
 class Solution:
     def countPairs(self, deliciousness: List[int]) -> int:
-        res=0
-        temp_dict={}
-
-        for i in deliciousness:
-            if len(temp_dict)==0:
-                temp_dict[i]=0
-            for j in range(22):
-                if (2**j)-i in temp_dict:
-                    res+=temp_dict[(2**j)-i]
-            if i not in temp_dict:
-                temp_dict[i]=1
-            elif i in temp_dict or temp_dict[i]==0:
-                temp_dict[i]+=1
-        return res % ((10**9)+7)
+        power = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576,2097152]
+        dic=Counter(deliciousness)
+        re=0
+        for i,j in dic.items():
+            for ele in power:
+                sub = ele - i
+                if sub in dic and sub == i:
+                    re+=j*(j-1)//2
+                elif sub in dic and sub != i:
+                    re+=j*dic[sub]/2
+        return int(re)%(10**9 +7)
