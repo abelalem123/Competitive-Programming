4214ef305099696e864e4b02cfbdef2003174bad
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-01-19 21:07:08 +0300

    Time: 1 ms (100.00%), Space: 43.9 MB (81.94%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+       
+        if (nums.length < 2)
+		return nums.length;
+ 
+	int j = 0;
+	int i = 1;
+ 
+	while (i < nums.length) {
+		if (nums[i] != nums[j]) {
+			j++;
+			nums[j] = nums[i];
+		}
+ 
+                i++;
+	}
+ 
+	return j + 1;
+    }
+}
