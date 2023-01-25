4027253470f3c8ca1913b5e82fd0bc6c340b1e41
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-01-25 20:27:39 +0300

    Time: 345 ms (32.31%), Space: 14.3 MB (80.73%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,13 +1,20 @@
+# class Solution:
+#     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+#         for j in range(len(names)):
+#             for i in range(1,len(names)-j):   
+#                 if heights[i-1]<heights[i]:
+#                     temp=names[i-1]
+#                     names[i-1]=names[i]
+#                     names[i]=temp
+#                     tempn=heights[i-1]
+#                     heights[i-1]=heights[i]
+#                     heights[i]=tempn
+#         return names
+
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for j in range(len(names)):
-            for i in range(1,len(names)-j):
-                
-                if heights[i-1]<heights[i]:
-                    temp=names[i-1]
-                    names[i-1]=names[i]
-                    names[i]=temp
-                    tempn=heights[i-1]
-                    heights[i-1]=heights[i]
-                    heights[i]=tempn
-        return names
+        hgts=sorted(heights,reverse=True)
+        p=[]
+        for i in hgts:
+            p.append(names[heights.index(i)])
+        return p
