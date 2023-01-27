33542586c2ad11ad3240fd8e51733de4dec847bc
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-01-27 21:11:08 +0300

    Time: 124 ms (71.06%), Space: 14.3 MB (80.90%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,16 +1,3 @@
-# class Solution:
-#     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-#         for j in range(len(names)):
-#             for i in range(1,len(names)-j):   
-#                 if heights[i-1]<heights[i]:
-#                     temp=names[i-1]
-#                     names[i-1]=names[i]
-#                     names[i]=temp
-#                     tempn=heights[i-1]
-#                     heights[i-1]=heights[i]
-#                     heights[i]=tempn
-#         return names
-
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         height_dict = dict(zip(heights,names)) # // height_dict = {180: 'Mary', 165: 'John', 170: 'Emma'}
