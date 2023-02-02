f4d97ed5be83da02c691c7d73739303f0e6d5331
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-02-02 20:17:51 +0300

    Time: 175 ms (34.22%), Space: 18.1 MB (80.36%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -5,11 +5,11 @@ class Solution:
         n=len(intervals)
         while i<n-1:
             if intervals[i][1]>=intervals[i+1][0] and intervals[i][0]<=intervals[i+1][1]:
-                ele= min(intervals[i][0],intervals[i+1][0])
-                ele2= max(intervals[i][1],intervals[i+1][1])
+                num1= min(intervals[i][0],intervals[i+1][0])
+                num2= max(intervals[i][1],intervals[i+1][1])
                 intervals.pop(i)
                 intervals.pop(i)
-                intervals.insert(i,[ele,ele2])
+                intervals.insert(i,[num1,num2])
                 n=n-1
             else:
                 i=i+1
