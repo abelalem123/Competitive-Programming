ab0ce5c3ac46494ccd387f8fd6caddd0b7a96fd0
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-02-13 21:38:42 +0300

    Time: 38 ms (68.42%), Space: 13.9 MB (63.70%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def multiply(self, num1: str, num2: str) -> str:
+
+        dict={'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+
+        n1=0
+
+        n2=0
+
+        for i in num1:
+
+            if i in dict:
+
+                n1=n1*10+dict[i]
+
+        for i in num2:
+
+            if i in dict:
+
+                n2=n2*10+dict[i]
+
+        return str(n1*n2)
