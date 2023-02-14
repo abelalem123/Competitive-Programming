ade883c8da30b0cb1a62efb0a8696fb11d74ec50
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-02-14 20:58:50 +0300

    Time: 31 ms (89.53%), Space: 13.9 MB (81.80%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+
+    def longestCommonPrefix(self, strs):
+
+        result = ""       
+
+        for n in zip(*strs):
+
+            if len(set(n)) == 1:
+
+                result += n[0]
+
+            else:
+
+                return result
+
+        return result  
