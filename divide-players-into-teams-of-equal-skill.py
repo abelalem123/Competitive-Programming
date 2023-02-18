6871a3194452d75f95f20d33579881078290f351
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-02-18 20:08:17 +0300

    Create divide-players-into-teams-of-equal-skill.py

---
+++ divide-players-into-teams-of-equal-skill.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        if len(skill)==2:
+            return skill[0]*skill[1]
+        skill.sort()
+        teams=[]
+        l=0
+        r=len(skill)-1
+        while l<r:
+            teams.append([skill[l],skill[r]])
+            l+=1
+            r-=1
+        sumx=teams[0][0]*teams[0][1]
+        for i in range(1,len(teams)):
+            if sum(teams[i-1])!=sum(teams[i]):
+                return -1
+            sumx+=teams[i][0]*teams[i][1]
+        return sumx
