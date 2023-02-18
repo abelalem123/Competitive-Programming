50ba755c71a9d2f965aafba816b2f28ba1859d50
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-02-18 20:16:46 +0300

    Create partition-labels.py

---
+++ partition-labels.py
@@ -0,0 +1,19 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        indexs=[0]*26
+        start=0
+        end=0
+        res=[]
+        
+        for i in range(len(s)):
+            indexs[ord(s[i])-97]=i
+        for i in range(len(s)):
+            end=max(indexs[ord(s[i])-97],end)
+            if i==end:
+                res.append(i+1-start)
+                start=i+1
+        return res
+            
+            
+            
+            
