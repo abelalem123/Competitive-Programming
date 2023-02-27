0587adb54c50ad99bd4f38285b8ea11d64fb3832
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-02-27 14:00:04 +0300

    Create rotating-the-box.py

---
+++ rotating-the-box.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        for row in box:
+            move_position = len(row) - 1             
+            for j in range(len(row) - 1, -1, -1):   
+                if row[j] == "*":                    
+                    move_position = j - 1           
+                elif row[j] == "#":                  
+                    row[move_position], row[j] = row[j], row[move_position]
+                    move_position -= 1
+
+        return zip(*box[::-1]) 
