7dc4350c197890f4f39c383688dbe26c2672ab67
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-03-10 20:31:15 +0300

    Time: 31 ms (69.89%), Space: 13.9 MB (49.44%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -1,19 +1,12 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        # stack=[]
-        # ans,val=0,0
-        # for i in s:
-        #     if i=="(":
-        #         stack.append(0)
-        #     elif i==")":
-        #         mul=stack.pop()
-        #         if mul==0:
-        #             val=1
-        #         else:
-        #             val=mul*2
-        #         if not stack:
-        #             ans+=val
-        #         else:
-        #             stack[-1]+=val
-        #     return ans
-        return eval(s.replace(")(",")+(").replace("()","1").replace(")",")*2"))
+        stack = []
+        cur = 0
+        for c in s:
+            if c == '(':
+                stack.append(cur)
+                cur = 0
+            else:
+                cur = stack.pop() + max(1, cur*2)
+        return cur 
+        # return eval(s.replace(")(",")+(").replace("()","1").replace(")",")*2"))
