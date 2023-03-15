56242ce82e65eab4bfebea3447759adc681a0d3f
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-03-15 20:37:31 +0300

    Time: 5643 ms (15.60%), Space: 13.8 MB (75.57%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,16 +1,16 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        @lru_cache(maxsize=1000)
-        def take(turn,i,j):
-            if i==j:
-                return turn * nums[i] 
-
-            choice_i= turn * nums[i] + take(-turn,i+1,j)
-            choice_j= turn * nums[j] + take(-turn,i,j-1)
-            if turn==1:
-                return max(choice_i,choice_j)
-            return min(choice_i,choice_j)
         
-           # return turn * max(turn *choice_i,turn*choice_j)
-        ans = take(1,0,len(nums)-1)
-        return ans >= 0
+        def rec(turn,l,r):
+            #p1 postive
+            if l==r:
+                return turn*nums[l]
+            left=turn*nums[l]+rec(-turn,l+1,r)
+            right=turn*nums[r]+rec(-turn,l,r-1)
+            
+            if turn==1:
+                return max(left,right)
+            else:
+                return min(left,right)
+        res=rec(1,0,len(nums)-1)
+        return res>=0
