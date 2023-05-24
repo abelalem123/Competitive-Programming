8720a36acbf43826265d19bbb2720118b7391b1f
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-05-24 20:45:27 +0300

    Time: 63 ms (5.43%), Space: 17 MB (14.69%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -5,35 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-       def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
-        queue = []
-        result = []
-
-        # If the root is not null, add it to the queue
-        if root:
-            queue.append(root)
-
-        # While the queue is not empty, repeat steps 4-7
-        while queue:
-            # Get the size of the queue and initialize an empty list to store the nodes of the current level
-            size = len(queue)
-            level = []
-
-            # Loop through the elements of the current level and add them to the list
-            for i in range(size):
-                node = queue.pop(0)
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans=[]
+        q=deque([root])
+        if not root:
+            return []
+        while q:
+            n=len(q)
+            level=[]
+            for _ in range(n):
+                node=q.popleft()
+                if not node:
+                    break
                 level.append(node.val)
-
-                # For each element of the current level, add its children to the queue
                 if node.left:
-                    queue.append(node.left)
+                    q.append(node.left)
                 if node.right:
-                    queue.append(node.right)
-
-            # Add the current level list to the result list
-            result.append(level)
-
-        # Return the result list
-        return result
+                    q.append(node.right)
+            ans.append(level)
+        return ans
