6dff135513ae1140e36431ebebc7edc6c7ec1813
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-06-04 12:51:40 +0300

    Time: 176 ms (22.07%), Space: 18.5 MB (25.85%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,40 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        [["X","O","X","O","X","O"],
+        ["O","X","O","X","O","X"],
+        ["X","O","X","O","X","O"],
+        ["O","X","O","X","O","X"]]
+        """
+        n=len(board)
+        m=len(board[0])
+        directions=[(-1,0),(1,0),(0,1),(0,-1)]
+        visited=set()
+        def inbound(row,col):
+            return 0<=row<len(board) and 0<=col<len(board[0])
+        def dfs(row,col):
+            board[row][col]='T'
+            for dr,dc in directions:
+                nr=dr+row
+                nc=dc+col
+                if inbound(nr,nc) and (nr,nc) not in visited and board[nr][nc]=='O' :
+                    visited.add((nr,nc))
+                    dfs(nr,nc)
+                    
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if i==0 or i==n-1 or j==0 or j==m-1:
+                    if board[i][j]=="O":
+                        dfs(i,j)
+                        
+        for i in range(n):
+            for j in range(m):
+                if board[i][j]=='O':
+                    board[i][j]='X'
+        for i in range(n):
+            for j in range(m):
+                if board[i][j]=='T':
+                    board[i][j]='O'
+        
+        
