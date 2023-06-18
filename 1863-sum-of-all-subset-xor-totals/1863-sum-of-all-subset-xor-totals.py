a30941ee38b4fe2034421d7bb445f0f0c8e893c1
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-06-18 15:44:19 +0300

    Time: 129 ms (27.32%), Space: 17 MB (9.16%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -4,19 +4,13 @@ class Solution:
         sub=[]
         def dfs(i):
             if i>=len(nums):
-                total.append(sub.copy())
+                if sub:
+                    total.append(reduce(lambda a, b: a^b, sub))
                 return
             sub.append(nums[i])
             dfs(i+1)
             sub.pop()
             dfs(i+1)
         dfs(0)
-        ans=0
-        for i in total:
-            if len(i)>0:
-                s=i[0]
-                
-                for j in range(1,len(i)):
-                    s^=i[j]
-                ans+=s
-        return ans
+        
+        return sum(total)
