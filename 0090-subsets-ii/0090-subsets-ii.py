5b78153e488005eaf49f101b1af7b048d3a5e4ea
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-06-19 13:48:03 +0300

    Time: 51 ms (64.88%), Space: 16.6 MB (18.89%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,22 +1,21 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        total=set()
+        total=[]
         sub=[]
         nums=sorted(nums)
         def dfs(i):
             if i>=len(nums):
-                total.add(' '.join(sub.copy()))
+                total.append(sub.copy())
                 return
             
-            sub.append(str(nums[i]))
+            sub.append(nums[i])
             dfs(i+1)
             sub.pop()
+            while i+1<len(nums) and nums[i]==nums[i+1]:
+                i+=1
             dfs(i+1)
        
         dfs(0)
         print(total)
-        ans=[]
-        for i in total:
-            x=list(map(int,i.split()))
-            ans.append(x)
-        return ans
+        return total
+       
