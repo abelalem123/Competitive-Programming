0074eb96188362c8e37e66b2e2c5542418c2af98
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-06-25 15:09:03 +0300

    Time: 2627 ms (30.83%), Space: 17 MB (94.74%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.py
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.py
@@ -1,23 +1,23 @@
 class Solution:
-    def countRoutes(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
-        n = len(locations)
+    MOD = 1000000007
 
-        memo = {}
-        def solve(currCity, remainingFuel):
-            if remainingFuel < 0:
-                return 0
-            if (currCity, remainingFuel) in memo:
-                return memo[(currCity, remainingFuel)]
-
-            ans = 0
-            if currCity == finish:
-                ans = 1
-            for nextCity in range(n):
-                if nextCity != currCity:
-                    ans = (ans + solve(nextCity, remainingFuel - abs(
-                        locations[currCity] - locations[nextCity]))) % 1000000007
-            
-            memo[(currCity, remainingFuel)] = ans
-            return ans
+    def helper(self, locations, city, finish, remainFuel, memo):
+        if remainFuel < 0:
+            return 0
         
-        return solve(start, fuel)
+        if memo[city][remainFuel] is not None:
+            return memo[city][remainFuel]
+        
+        total = 1 if city == finish else 0
+        
+        for nextCity in range(len(locations)):
+            if nextCity != city and remainFuel >= abs(locations[nextCity] - locations[city]):
+                total = (total + self.helper(locations, nextCity, finish, remainFuel - abs(locations[nextCity] - locations[city]), memo)) % self.MOD
+        
+        memo[city][remainFuel] = total
+        return total
+
+    def countRoutes(self, locations, start, finish, fuel):
+        n = len(locations)
+        memo = [[None] * (fuel + 1) for _ in range(n)]
+        return self.helper(locations, start, finish, fuel, memo)
