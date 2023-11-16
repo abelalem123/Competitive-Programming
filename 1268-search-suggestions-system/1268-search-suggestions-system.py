9b704a996be5497d9a2b41bd02989f81c3022e01
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2023-11-16 09:46:58 +0300

    Time: 405 ms (21.26%), Space: 19.4 MB (75.87%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -0,0 +1,11 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        product = []
+        for i in range(len(searchWord)):
+            p = []
+            for prod in products:
+                if prod.startswith(searchWord[:i+1]):
+                    p.append(prod)
+            p = sorted(p)[:3]
+            product.append(p)
+        return product
